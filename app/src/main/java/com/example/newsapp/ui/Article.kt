e0a850990c4974cd0199e4090708ce13513c4d50
zae21 <prayoga96@gmail.com>
2022-11-01 15:37:26 +0700

    add dao article

--- app/src/main/java/com/example/newsapp/ui/Article.kt
+++ app/src/main/java/com/example/newsapp/ui/Article.kt
@@ -1,6 +1,14 @@
 package com.example.newsapp.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int? = null,
     val author: String,
     val content: String,
     val description: String,
