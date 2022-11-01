4a8f3e494f06af6addf2135311f6095db5282161
zae21 <prayoga96@gmail.com>
2022-11-01 15:13:22 +0700

    updae

---
+++ app/src/main/java/com/example/newsapp/ui/api/NewsApi.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp.ui.api
+
+import com.example.newsapp.ui.NewsResponse
+import com.example.newsapp.ui.util.Constant.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode: String = "us",
+        @Query("page") pageNumber : Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ) : Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber : Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ) : Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/ui/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.ui.util
+
+class Constant {
+    companion object {
+        const val API_KEY = "3b1a313d9a8d46e7b3d3dc1e896f28e7"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
