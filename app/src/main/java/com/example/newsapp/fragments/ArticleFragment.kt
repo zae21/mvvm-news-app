c5ab4219c545443cd769243585388022596183c4
zae21 <prayoga96@gmail.com>
2022-11-01 17:21:27 +0700

    add prepare

--- app/src/main/java/com/example/newsapp/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/ArticleFragment.kt
@@ -1,6 +1,17 @@
 package com.example.newsapp.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
+import com.example.newsapp.ui.NewsActivity
+import com.example.newsapp.ui.NewsViewModel
 
-class ArticleFragment : Fragment(R.layout.fragment_article) {}
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
+}
--- app/src/main/java/com/example/newsapp/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/BreakingNewsFragment.kt
@@ -1,6 +1,17 @@
 package com.example.newsapp.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
+import com.example.newsapp.ui.NewsActivity
+import com.example.newsapp.ui.NewsViewModel
 
-class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {}
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
+}
